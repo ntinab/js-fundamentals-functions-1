d59663346121d2341e056334a1e7f6a3165a17a6
Ntina B <74467955+ntinab@users.noreply.github.com>
2023-08-13 16:25:42 +0300

    ok .

--- src/extensions/creating-functions-multiple-args.js
+++ src/extensions/creating-functions-multiple-args.js
@@ -1,42 +1,49 @@
-// 1. define a function that takes two numbers: lower and upper.
-// The function should return an array containing all the numbers between
-// lower and upper, including lower and upper.
-//
-// Example Input and Output:
-// Input        | Output
-// 1, 3         | [1,2,3]
-// 10, 13       | [10, 11, 12, 13]
-// -1, 1        | [-1, 0, 1]
-//
-// TODO: write code below
-
-// 2. define a function that takes two arguments: a string and a number.
-// The function should return the same string but in upper case with exclamation
-// marks appended to the end. The number of exclamation marks should be
-// determined by the number argument.
-//
-// Example Input and Output:
-// Input        | Output
-// disaster, 5  | DISASTER!!!!!
-// error, 10    | ERROR!!!!!!!!!!
-//
-// TODO: write code below
-
-// 3. define a function that takes two arguments: a string and a number.
-// The function should return the new time in 24hr time after adding the additional time in minutes.
-//
-// Example Input and Output:
-// Input        | Output
-// '7:50', 4    | '7:54'
-// '7:50', 72   | '9:02'
-// '11:50', 20  | '12:10'
-// '12:50', 120 | '14:50'
-// '23:50', 30  | '00:20'
-// TODO: write code below
-
-// TODO: change the exported value to be the name of the function you defined
-module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
-  b: undefined, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
-  c: undefined // etc
+// 1. Define a function that creates a range of numbers between 'lower' and 'upper'.
+function createNumberRange(lower, upper) {
+  const result = [];
+  for (let i = lower; i <= upper; i++) {
+    result.push(i);
+  }
+  return result;
 }
+
+// 2. Define a function that converts a string to uppercase and appends exclamation marks.
+function addExclamationMarks(str, num) {
+  const uppercaseStr = str.toUpperCase();
+  const exclamationMarks = '!'.repeat(num);
+  return uppercaseStr + exclamationMarks;
+}
+
+// 3. Define a function that adds minutes to a given time in 24hr format.
+function addMinutesToTime(time, minutes) {
+  const [hourStr, minuteStr] = time.split(":");
+  let hour = parseInt(hourStr);
+  let minute = parseInt(minuteStr);
+
+  // Add the minutes
+  minute += minutes;
+
+  // Adjust hours and minutes if necessary
+  hour += Math.floor(minute / 60);
+  minute %= 60;
+
+  // Normalize hour to a 24-hour format
+  hour %= 24;
+
+  // Format the time strings with leading zeros
+  const formattedHour = hour < 10 ? `${hour}` : `${hour}`;
+  const formattedMinute = minute < 10 ? `0${minute}` : `${minute}`;
+
+  // Handle the case when hour is 0
+  if (formattedHour === '0') {
+    return `00:${formattedMinute}`;
+  }
+
+  return `${formattedHour}:${formattedMinute}`;
+}
+
+module.exports = {
+  a: createNumberRange,
+  b: addExclamationMarks,
+  c: addMinutesToTime
+};
